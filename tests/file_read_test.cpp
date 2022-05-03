8cae845e4fe4bbb243c718340748e9023c8d05a0
Charles Lewis <calewis5@illinois.edu>
2022-05-03 00:01:00 +0000

    finished adjacency list tests

--- tests/file_read_test.cpp
+++ tests/file_read_test.cpp
@@ -9,6 +9,9 @@
 
 using namespace std;
 
+
+// ADJACENCY MATRIX TESTS
+
 TEST_CASE("vertices_sizecheck_small")
 {
     file_Reader test;
@@ -88,4 +91,87 @@ TEST_CASE("getEdge_large5")
     test.readingTextFile("/workspaces/CS225/cs225git/calewis5-mmalysa2-akales3-mkumar41/file.txt");
 
     REQUIRE(test.getAdj()[269][398] == 37);
+}
+
+
+//ADJACENCY LIST TESTS
+
+TEST_CASE("vertices_list_size_check_small")
+{
+    // tests that the amount of vertices in adjacency list is correct
+    file_Reader test;
+    test.readingTextFile("/workspaces/CS225/cs225git/calewis5-mmalysa2-akales3-mkumar41/tests/testDataCleaning.txt");
+
+    REQUIRE(test.getAdjList().size() == 5);
+}
+
+
+TEST_CASE("edges_list_size_check_small")
+{
+    // tests that the amount of edges in adjacency list is correct
+    file_Reader test;
+    test.readingTextFile("/workspaces/CS225/cs225git/calewis5-mmalysa2-akales3-mkumar41/tests/testDataCleaning.txt");
+    int edgeCount = 0;
+    for (unsigned int i = 0; i < test.getAdjList().size(); i++) {
+        edgeCount = edgeCount + test.getAdjList()[i].size();
+    }
+    //should be 12 total edges for this graph
+    REQUIRE(edgeCount == 12);
+}
+
+TEST_CASE("vertices_list_size_check_large")
+{
+    // tests that the amount of vertices in adjacency list is correct
+    file_Reader test;
+    test.readingTextFile("/workspaces/CS225/cs225git/calewis5-mmalysa2-akales3-mkumar41/file.txt");
+
+    REQUIRE(test.getAdjList().size() == 456);
+}
+
+TEST_CASE("edges_list_size_check_large")
+{
+    // tests that the amount of edges in adjacency list is correct
+    file_Reader test;
+    test.readingTextFile("/workspaces/CS225/cs225git/calewis5-mmalysa2-akales3-mkumar41/file.txt");
+    unsigned int edgeCount = 0;
+    for (unsigned int i = 0; i < test.getAdjList().size(); i++) {
+        edgeCount = edgeCount + test.getAdjList()[i].size();
+    }
+    //should be 71959 total edges for this graph
+    REQUIRE(edgeCount == 71959);
+}
+
+
+TEST_CASE("arbitrary_connections_small")
+{
+    //tests certain edges & weights to make sure they are correct
+    file_Reader test;
+    test.readingTextFile("/workspaces/CS225/cs225git/calewis5-mmalysa2-akales3-mkumar41/tests/testDataCleaning.txt");
+    REQUIRE(test.getAdjList()[0][0].first == 1);
+    REQUIRE(test.getAdjList()[0][0].second == 100);
+
+
+    REQUIRE(test.getAdjList()[3][0].first == 0);
+    REQUIRE(test.getAdjList()[3][0].second == 300);
+
+    REQUIRE(test.getAdjList()[2][2].first == 4);
+    REQUIRE(test.getAdjList()[2][2].second == 600);
+}
+
+
+
+TEST_CASE("arbitrary_connections_large")
+{
+    //tests certain edges & weights to make sure they are correct
+    file_Reader test;
+    test.readingTextFile("/workspaces/CS225/cs225git/calewis5-mmalysa2-akales3-mkumar41/file.txt");
+    REQUIRE(test.getAdjList()[444][0].first == 1);
+    REQUIRE(test.getAdjList()[444][0].second == 322);
+
+
+    REQUIRE(test.getAdjList()[252][1].first == 3);
+    REQUIRE(test.getAdjList()[252][1].second == 237);
+
+    REQUIRE(test.getAdjList()[365][8].first == 32);
+    REQUIRE(test.getAdjList()[365][8].second == 203);
 }
